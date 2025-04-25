c742549a24da4c01c98e2adc76050fa12c95cc70
Liam Fries <liamspersonalemail@gmail.com>
2025-04-25 11:25:54 -0400

    Added option for keyboard number based controller

--- ControllerTest.py
+++ ControllerTest.py
@@ -28,9 +28,21 @@ class GameController(ABC):
         return self._circles
     
     @abstractmethod
-    def move(self):
+    def get_first_point(self):
         """
         """
+
+    @abstractmethod
+    def get_second_point(self):
+        """
+        """
+
+    @abstractmethod
+    def get_number(self):
+        """
+        """
+
+    
 
 class MouseController(GameController):
     """
@@ -115,4 +127,68 @@ class MouseController(GameController):
 # The list of circle coordinates and circle sizes
 # The list of connections of nodes
 # The number of blips at each point
+class KeyController(GameController):
+    """
+    """
 
+    def get_circle(self):
+        """
+        Checks if the cursor is over a viable circle
+
+        Returns:
+            viable_pos: a bool representing if the cursor is in a viable spot
+            or not
+            index: the index of the circle that the cursor if over from
+            circle_data
+        """
+        index = input("Which circle do you want to select?")
+        try:
+            index = int(index)
+            if not isinstance(index, int):
+                raise ValueError
+            elif index < 0: 
+            #or index > len(connections):
+                raise ValueError
+        except(ValueError):
+            print("Input not in range or not a number")
+        return index
+
+    def get_number(self):
+        """
+        Get input number to move
+        """
+        number = input("How many units do you want to move")
+        try:
+            number = int(number)
+            if not isinstance(number, int):
+                raise ValueError
+            elif number < 0:
+                raise ValueError
+            #elif number > cirle_max
+                #raise ValueError
+        except(ValueError):
+            print("Input not in range or not a number")
+        return number
+    
+    def get_first_point(self):
+        """
+        Gets the origin of the blips to be moved
+        """
+        return self.get_circle()
+    
+    def get_second_point(self):
+        """
+         Get a second circle to move blips to. Checks if the circle is 
+         adjacent to the first one.
+        """   
+        connections = {
+            0: [1, 2], 1: [0, 2, 3, 4], 2: [0, 1, 3, 4],
+            3: [1, 2, 4, 5], 4: [1, 2, 3, 5], 5: [3, 4]
+        }
+        second_circle = self.get_circle()
+        if second_circle in connections[self.get_first_point()]:
+            return second_circle
+        else:
+            raise ValueError
+        #first circle/get_first_point will be overridden with in the game
+        #function with what we want
