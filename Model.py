493793bb4d80ec9aa12fde1bcc96da626310f82a
Liam Fries <liamspersonalemail@gmail.com>
2025-04-29 11:31:46 -0400

    Push some updates to model

--- Model.py
+++ Model.py
@@ -26,8 +26,42 @@ class Model:
             0: [1, 2], 1: [0, 2, 3, 4], 2: [0, 1, 3, 4],
             3: [1, 2, 4, 5], 4: [1, 2, 3, 5], 5: [3, 4]
         }
+        # Set who owns what point at the start. 0 is empty, 1 is player 1, etc.
+        self.owners = {0 : 1, 1 : 0, 2 : 0, 3 : 1, 4 : 0, 5 : 0}
+        # How many oliners are at each point at the start
+        self.oliners = [5, 5, 5, 5, 5, 5]
 
         self.running = True
+    
+    def add_oliners(self):
+        """
+        Adds an Oliner to each owned building
+        """
+        for index in range(len(self.oliners)):
+            if self.owners[index] > 0:
+                self.oliners[index] += 1
+
+    def send_oliners(self, first_point, second_point, number_to_move):
+        """
+        Works out how Oliners are sent from building to building. Changes who
+        owns what building
+        NOTE: We can't input directly from controller to here within the class
+        without changing inheritance structures. We can add the inputs in the
+        update function instead, outside of the class
+        """
+        player = self.owners[first_point]
+        self.oliners[first_point] -= number_to_move
+        if self.owners[second_point] == player:
+            self.oliners[second_point] += number_to_move
+        elif self.oliners[second_point] > number_to_move:
+            self.oliners[second_point] -= number_to_move
+        elif self.oliners[second_point] == number_to_move:
+            self.oliners[second_point] = 0
+            self.owners[second_point] = player
+        elif self.oliners[second_point] < number_to_move:
+            self.oliners[second_point] = number_to_move - self.oliners[second_point]
+            self.owners[second_point] = player
+        
 
     def calculate_edge_point(self, x1, y1, x2, y2, radius):
         """Calculate the point on the edge of a circle closest to another point."""
